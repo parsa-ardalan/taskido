565c7c8ee9bc74161103aa6046b5bec676e74eac
Parsa Ardalan <parsajsprogrammer@gmail.com>
2026-06-30 16:35:59 +0330

    redux persist installed. now notes and routine will be stored in local storage

--- src/redux/store/provider.jsx
+++ src/redux/store/provider.jsx
@@ -1,8 +1,27 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import { Provider } from 'react-redux'
-import { store } from '@/redux/store/store'
+import { PersistGate } from 'redux-persist/integration/react'
+
+import { store, persistor } from '@/redux/store/store'
 
 export default function Providers({ children }) {
-    return <Provider store={store}>{children}</Provider>
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return null
+    }
+
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                {children}
+            </PersistGate>
+        </Provider>
+    )
 }
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,12 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+
+import {
+    persistStore,
+    persistReducer,
+} from 'redux-persist'
+
+import storage from 'redux-persist/lib/storage'
 
 // slices
 import notesSlice from "@/redux/notes/notesSlice"
 import routineSlice from '../routine/routineSlice'
 
-export const store = configureStore({
-    reducer: {
-        notes: notesSlice,
-        routine: routineSlice
-    },
+const rootReducer = combineReducers({
+    notes: notesSlice,
+    routine: routineSlice,
 })
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['notes', 'routine'],
+}
+
+const persistedReducer = persistReducer(
+    persistConfig,
+    rootReducer
+)
+
+export const store = configureStore({
+    reducer: persistedReducer,
+
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+})
+
+export const persistor = persistStore(store)
